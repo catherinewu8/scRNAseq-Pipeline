9f1f9a469512d24e4084be7c411becded5664b71
Catherine Wu <catherinewu2000@gmail.com>
2026-07-01 14:26:46 -0400

    Create 02_prefetch.sh
    Download SRR from list or individual run.

---
+++ 02_prefetch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#--- Download SRR from NCBI ---#
+
+# Uncomment the line below if you want to download all the runs from the list
+# prefetch --option-file SRR_list.txt --max-size 100G
+
+# Uncomment the line below if you want to download a single run
+# prefetch SRR19432685 --max-size 100G
