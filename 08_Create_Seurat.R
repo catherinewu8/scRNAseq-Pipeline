eccbbd87a6b0696250a9b8bd9a16a736ef639d63
Catherine Wu <catherinewu2000@gmail.com>
2026-09-03 17:05:14 -0400

    Update 08_Create_Seurat.R

--- 08_Create_Seurat.R
+++ 08_Create_Seurat.R
@@ -3,3 +3,18 @@ library(tidyverse)
 library(Seurat)
 
 #---Load Data---#
+
+dirs <- list.dirs(
+  getwd(), 
+  recursive = FALSE, 
+  full.names = FALSE
+)
+
+#---Build Seurat Objects---#
+
+gse <- "" # Enter GSE ID here
+
+sample_dirs <- dirs[grep1(paste0(gse,"_SRR"), dirs)]
+
+seurat_list <- list()
+
